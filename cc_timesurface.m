ba4742204143610f38f59c87c4ce776acf03cffa
Colleen Pam Chen <colleenpamchen@gmail.com>
2017-03-21 17:56:39 -0700

    added surface plotting

--- cc_timesurface.m
+++ cc_timesurface.m
@@ -1,5 +1,6 @@
 %% colleen's working progress 
-[CIN]=loadaerdat_matlab('DVS.aedat');
+% [CIN]=loadaerdat_matlab('DVS128-2017-03-21_penOrientation.aedat');
+[CIN]=loadaerdat_matlab('DVS128-2017-03-21T16-00-20-0700-0.aedat');
 
 % Column 1: timestamps with 1us time tick
 % Columns 2-3: ignore them (they are meant for simulator AERST (see Perez-Carrasco et al, IEEE
@@ -7,8 +8,10 @@
 % Column 4: x coordinate (from 0 to 127)
 % Column 5: y coordinate (from 0 to 127)
 % Column 6: event polarity
+% [ x-coordinate , y-coordinate , polarity , timestamp (microseconds) ]
 
 n=size(CIN,1);
+nn = ceil(n / 100) ; 
 
 ts= CIN(:,1); 
 xx=CIN(:,4);
@@ -16,34 +19,48 @@ yy=CIN(:,5);
 xy=CIN(:,4:5); 
 P=CIN(:,6);
 
-S = NaN(128);
+Son = nan(128);
+Soff = nan(128);
+
 deltaT = zeros(128);
 ts_prev=0; 
-tau = 1 ; 
-
+tau = 50000 ; % from paper: 50 milliseconds, conver this into microseconds. 
 % this loops through the entire recording. 
 % TODO: change it so that it takes incoming data one by one 
-   
-for i = 1:n
+
+for i = 1: n
     
    x=xx(i)+1; % fix MATLAB's indexing starting with 1 
    y=yy(i)+1; 
    ts_current = ts(i); 
    
    if P(i)==1 % for ON polarity events only 
+       deltaT = deltaT + (ts_current - ts_prev);
+       ts_prev = ts_current; 
+       deltaT(x,y)=0; 
+       Son = exp( -(deltaT)/tau )  ; 
+      % for a location (x,y), take the current time at THAT location minus previous time at that location  
+   end  
    
-   deltaT = deltaT + (ts_current - ts_prev);
-   ts_prev=ts_current; 
-   deltaT(x,y)=0; 
-   S= exp( -(deltaT)/tau )  ; 
-  % for a location (x,y), take the current time at THAT location minus previous time at that location  
-  
-   end
+%    if P(i)==-1 % for OFF polarity events only 
+%        deltaT = deltaT + (ts_current - ts_prev);
+%        ts_prev=ts_current; 
+%        deltaT(x,y)=0; 
+%        Soff= exp( -(deltaT)/tau )  ; 
+%       % for a location (x,y), take the current time at THAT location minus previous time at that location  
+%    end  
+%    imagesc(S)
+%    F(i)= getframe; 
    
-       
 end
 
-imagesc(S)
+surf(Son) 
+
+
+% movie(F)
+
+% imagesc(S)
+
 
 
 
