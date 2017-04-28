65a983da9c1fee50cd6e77a0edd7095e304c8af1
Colleen Pam Chen <colleenpamchen@gmail.com>
2017-04-27 17:04:51 -0700

    hots function now works and plots the batched events

---
+++ cc_hots_main.m
@@ -0,0 +1,39 @@
+%% PRE-RECORDED DVS DATASET from Jorg Conradt 
+% data already in matrix format: 
+    % Column 1: x coordinate (from 0 to 127) 
+    % Column 2: y coordinate (from 0 to 127)
+    % Column 3: event polarity [0 off | 1 on]
+    % Column 4: timestamps with 1us time tick (microseconds?miliseconds? how to convert to seconds?)
+    % Events = [ x-coordinate , y-coordinate , polarity , timestamp (microseconds) ]
+Events = load('sample_eDVS_data/pen_vertical.dvs');
+% Events = load('sample_eDVS_data/pen_horizontal.dvs');
+% Events = load('sample_eDVS_data/spinner.dvs');
+% Events = load('sample_eDVS_data/hand_vertical.dvs');
+% Events = load('sample_eDVS_data/hand_horizontal.dvs');
+    
+%% video visualizing the events 
+visualize_events(Events); 
+
+%% Batch incoming events: 
+num_events = size(Events,1) ; 
+batchsize = 1000;
+end_video = floor(num_events/batchsize) ;
+
+
+idx=[1:batchsize];
+for i = 1:   end_video
+    data = Events(idx,:); 
+%     visualize_events(data); 
+    
+    [CON, COFF]=hots(data); 
+
+
+
+    idx = idx+batchsize; 
+%     pause 
+
+end
+
+
+
+
--- hots.m
+++ hots.m
@@ -1,20 +1,15 @@
-function [ CON, COFF ] = hots(CIN, vstart, vend )
+function [ CON, COFF ] = hots(events)
 
 % Parse the CIN input event into component parts, and truncate video 
-    n = size(CIN,1);
-    ts = CIN(:,1); 
-    x0 = CIN(:,4);
-    y0 = CIN(:,5);
-    xy0 = CIN(:,4:5); 
-    P = CIN(:,6);
+    N = size(events,1);
+    x0 = events(:,1); 
+    y0 = events(:,2);
+    P = events(:,3);
+    ts = events(:,4); 
+   
 % initialize constant variables:
 pixels= 128; 
 
-for k=1:size(P,1)
-    if P(k) == -1
-       P(k) = 0;
-    end
-end
     logical_pol = logical(P);
     % ON polarity events 
     x_incr = x0(logical_pol);
@@ -28,16 +23,16 @@ end
     res_x = pixels;
     res_y = pixels;
 
-%% ON polarity events 
+% ON polarity events 
     Son = nan(pixels);
     ONS=[]; 
     deltaTon = zeros(pixels);
     ts_prev=0; 
-    tau = 20000 ;
+    tau = 20000 ; %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% tau is a parameter!!! 
     x = x_incr+1;
     y = y_incr+1;
     % Build time surfaces for ON events 
-    for i = vstart:length(ts_incr)/1.25
+    for i = 1:length(ts_incr)
         
         ts_current = ts_incr(i) ; 
         deltaTon = deltaTon + (ts_current - ts_prev) ; 
@@ -47,7 +42,7 @@ end
         ONS(:,:,i) = Son ;
     end
 % PROTOTYPE construction using nearest neighbor and distance metrics 
-Cidx=[10:100:1049]; % N=11 
+Cidx=[1:10]; % Cn=10
 npixels= pixels*pixels; 
 Cn = reshape( permute( ONS(:,:,Cidx(:)), [3 1 2] ), [length(Cidx), npixels]);
 CON = zeros(size(Cn));
@@ -55,7 +50,7 @@ pk = 0;
 beta = -1;
 
 % ON polarity events loop
-for i=idxstart:size(ONS,3)
+for i=1:size(ONS,3)
 
     test_on_event = reshape(ONS(:,:,i),[1,npixels]); % Si 
     D_on = pdist2(Cn, test_on_event); 
@@ -75,35 +70,35 @@ for i=idxstart:size(ONS,3)
     CON=reshape(CON,[length(Cidx),128,128]);
      
    
-%  idx(1)
-% figure
-% subplot(1,2,1)
-% contour(ONS(:,:,i) )
-% title('test')
-% subplot(1,2,2)  
-% contour( squeeze(C(idx(1),:,:) ) ) 
-% title( sprintf('PROTOTYPE|train %d', idx(1)) )
+                        % idx(1)
+                        % figure
+                        % subplot(1,2,1)
+                        % contour(ONS(:,:,i) )
+                        % title('test')
+                        % subplot(1,2,2)  
+                        % contour( squeeze(C(idx(1),:,:) ) ) 
+                        % title( sprintf('PROTOTYPE|train %d', idx(1)) )
 
 end
 
-figure
-subplot(1,2,1)
-contour(ONS(:,:,i) )
-title('test')
-subplot(1,2,2)  
-contour( squeeze(CON(idx(1),:,:) ) ) 
-title( sprintf('PROTOTYPE|train %d', idx(1)) )
+                        % figure
+                        % subplot(1,2,1)
+                        % contour(ONS(:,:,i) )
+                        % title('test')
+                        % subplot(1,2,2)  
+                        % contour( squeeze(CON(idx(1),:,:) ) ) 
+                        % title( sprintf('PROTOTYPE|train %d', idx(1)) )
     
-%% OFF polarity events 
+% OFF polarity events 
     Soff = nan(pixels);
     OFFS=[]; 
     deltaToff = zeros(pixels);
     ts_prev=0; 
-    tau = 20000 ;
+%     tau = 20000 ;
     x = x_decr+1;
     y = y_decr+1;
     
-    for i = vstart:ts_decr(end) %length(ts_decr)/1.25
+    for i = 1:length(ts_decr)
     ts_current = ts_decr(i) ; 
     deltaToff = deltaToff + (ts_current - ts_prev) ; 
     ts_prev = ts_current;
@@ -111,36 +106,17 @@ title( sprintf('PROTOTYPE|train %d', idx(1)) )
     Soff = exp( -(deltaToff)/tau )  ;  
     OFFS(:,:,i) = Soff ;
     end
-
-% Display the surface and contounrs for ON and OFF events 
-% e=2050; % event snapshot 
-on = length(ts_incr);
-off= length(ts_decr);
-
-figure
-subplot(2,2,1)
-surf(ONS(:,:,on) )
-subplot(2,2,2)  
-contour(ONS(:,:,on)) 
-title('ON events')
-subplot(2,2,3)
-surf(OFFS(:,:,off) )
-subplot(2,2,4)  
-contour(OFFS(:,:,off)) 
-title('OFF events')
-
+    
 % OFF polarity events loop
 
-Cidx=[10:100:1049]; % N=11 
+Cidx=[1:10]; % Cn=10
 npixels = pixels*pixels; % 16384
 Cn = reshape( permute( OFFS(:,:,Cidx(:)), [3 1 2] ), [length(Cidx), npixels]);
 COFF = zeros(size(Cn));
 pk = 0; 
 beta = -1;  
 
-for i=idxstart:idxend
- 
-   
+for i=1:size(OFFS,3)
     test_off_event = reshape(OFFS(:,:,i),[1,npixels]);
     D_off = pdist2(Cn,test_off_event); 
         [val,idx]=sort(D_off);
@@ -157,30 +133,139 @@ for i=idxstart:idxend
     COFF(idx(1),:) = Ck; 
     COFF=reshape(COFF,[length(Cidx),128,128]);
      
-    idx(1)
+                                % idx(1)
+                                % figure
+                                % subplot(1,2,1)
+                                % contour(OFFS(:,:,i) )
+                                % title('test')
+                                % subplot(1,2,2)  
+                                % contour( squeeze(COFF(idx(1),:,:) ) ) 
+                                % title('PROTOTYPE|train') 
+end
+
+                                % figure
+                                % subplot(1,2,1)
+                                % contour(OFFS(:,:,i) )
+                                % title('test')
+                                % subplot(1,2,2)  
+                                % contour( squeeze(COFF(idx(1),:,:) ) ) 
+                                % title( sprintf('PROTOTYPE|train %d', idx(1)) )
+
+
+
+
+% Display the surface and contounrs for ON and OFF events 
+% e=2050; % event snapshot 
+on = length(ts_incr);
+off= length(ts_decr);
+
 % figure
-% subplot(1,2,1)
-% contour(OFFS(:,:,i) )
-% title('test')
-% subplot(1,2,2)  
-% contour( squeeze(C(idx(1),:,:) ) ) 
-% title('PROTOTYPE|train') 
-end
+% subplot(2,2,1)
+% surf(ONS(:,:,on) )
+% subplot(2,2,2)  
+% contour(ONS(:,:,on)) 
+% title('ON events')
+% subplot(2,2,3)
+% surf(OFFS(:,:,off) )
+% subplot(2,2,4)  
+% contour(OFFS(:,:,off)) 
+% title('OFF events')
 
+
+%% INITIAL prototypes : 
+figure; 
+subplot(ceil(length(Cidx)/2),2,1); contour( ONS(:,:,Cidx(1)))
+subplot(ceil(length(Cidx)/2),2,2); contour( ONS(:,:,Cidx(2)))
+subplot(ceil(length(Cidx)/2),2,3); contour( ONS(:,:,Cidx(3)))
+subplot(ceil(length(Cidx)/2),2,4); contour( ONS(:,:,Cidx(4)))
+subplot(ceil(length(Cidx)/2),2,5); contour( ONS(:,:,Cidx(5)))
+subplot(ceil(length(Cidx)/2),2,6); contour( ONS(:,:,Cidx(6)))
+subplot(ceil(length(Cidx)/2),2,7); contour( ONS(:,:,Cidx(7)))
+subplot(ceil(length(Cidx)/2),2,8); contour( ONS(:,:,Cidx(8)))
+subplot(ceil(length(Cidx)/2),2,9); contour( ONS(:,:,Cidx(9)))
+subplot(ceil(length(Cidx)/2),2,10); contour( ONS(:,:,Cidx(10)))
+suptitle('ON polarity initial prototypes')
+ 
+figure; 
+subplot(ceil(length(Cidx)/2),2,1); contour( OFFS(:,:,Cidx(1)))
+subplot(ceil(length(Cidx)/2),2,2); contour( OFFS(:,:,Cidx(2)))
+subplot(ceil(length(Cidx)/2),2,3); contour( OFFS(:,:,Cidx(3)))
+subplot(ceil(length(Cidx)/2),2,4); contour( OFFS(:,:,Cidx(4)))
+subplot(ceil(length(Cidx)/2),2,5); contour( OFFS(:,:,Cidx(5)))
+subplot(ceil(length(Cidx)/2),2,6); contour( OFFS(:,:,Cidx(6)))
+subplot(ceil(length(Cidx)/2),2,7); contour( OFFS(:,:,Cidx(7)))
+subplot(ceil(length(Cidx)/2),2,8); contour( OFFS(:,:,Cidx(8)))
+subplot(ceil(length(Cidx)/2),2,9); contour( OFFS(:,:,Cidx(9)))
+subplot(ceil(length(Cidx)/2),2,10); contour( OFFS(:,:,Cidx(10)))
+suptitle('OFF polarity initial prototypes')
+
+%% View the prototype : ON 
 figure
-subplot(1,2,1)
-contour(OFFS(:,:,i) )
-title('test')
-subplot(1,2,2)  
-contour( squeeze(COFF(idx(1),:,:) ) ) 
-title( sprintf('PROTOTYPE|train %d', idx(1)) )
+subplot(ceil(length(Cidx)/2),2,1)
+contour( squeeze(CON(1,:,:) ) ) 
+title( sprintf('ON PROTOTYPE 1' ))
+subplot(ceil(length(Cidx)/2),2,2)
+contour( squeeze(CON(2,:,:) ) ) 
+title( sprintf('ON PROTOTYPE 2' ))
+subplot(ceil(length(Cidx)/2),2,3)
+contour( squeeze(CON(3,:,:) ) ) 
+title( sprintf('ON PROTOTYPE 3'))
+subplot(ceil(length(Cidx)/2),2,4)
+contour( squeeze(CON(4,:,:) ) ) 
+title( sprintf('ON PROTOTYPE 4' ))
+subplot(ceil(length(Cidx)/2),2,5)
+contour( squeeze(CON(5,:,:) ) ) 
+title( sprintf('ON PROTOTYPE 5' ))
+subplot(ceil(length(Cidx)/2),2,6)
+contour( squeeze(CON(6,:,:) ) ) 
+title( sprintf('ON PROTOTYPE 6' ))
+subplot(ceil(length(Cidx)/2),2,7)
+contour( squeeze(CON(7,:,:) ) ) 
+title( sprintf('ON PROTOTYPE 7' ))
+subplot(ceil(length(Cidx)/2),2,8)
+contour( squeeze(CON(8,:,:) ) ) 
+title( sprintf('ON PROTOTYPE 8' ))
+subplot(ceil(length(Cidx)/2),2,9)
+contour( squeeze(CON(9,:,:) ) ) 
+title( sprintf('ON PROTOTYPE 9' ))
+subplot(ceil(length(Cidx)/2),2,10)
+contour( squeeze(CON(10,:,:) ) ) 
+title( sprintf('ON PROTOTYPE 10' ))
+suptitle('ON polarity Prototypes')
+
+%% View the prototype : OFF
+figure
+subplot(ceil(length(Cidx)/2),2,1)
+contour( squeeze(COFF(1,:,:) ) ) 
+title( sprintf('OFF PROTOTYPE 1' ))
+subplot(ceil(length(Cidx)/2),2,2)
+contour( squeeze(COFF(2,:,:) ) ) 
+title( sprintf('OFF PROTOTYPE 2' ))
+subplot(ceil(length(Cidx)/2),2,3)
+contour( squeeze(COFF(3,:,:) ) ) 
+title( sprintf('OFF PROTOTYPE 3'))
+subplot(ceil(length(Cidx)/2),2,4)
+contour( squeeze(COFF(4,:,:) ) ) 
+title( sprintf('OFF PROTOTYPE 4' ))
+subplot(ceil(length(Cidx)/2),2,5)
+contour( squeeze(COFF(5,:,:) ) ) 
+title( sprintf('OFF PROTOTYPE 5' ))
+subplot(ceil(length(Cidx)/2),2,6)
+contour( squeeze(COFF(6,:,:) ) ) 
+title( sprintf('OFF PROTOTYPE 6' ))
+subplot(ceil(length(Cidx)/2),2,7)
+contour( squeeze(COFF(7,:,:) ) ) 
+title( sprintf('OFF PROTOTYPE 7' ))
+subplot(ceil(length(Cidx)/2),2,8)
+contour( squeeze(COFF(8,:,:) ) ) 
+title( sprintf('OFF PROTOTYPE 8' ))
+subplot(ceil(length(Cidx)/2),2,9)
+contour( squeeze(COFF(9,:,:) ) ) 
+title( sprintf('OFF PROTOTYPE 9' ))
+subplot(ceil(length(Cidx)/2),2,10)
+contour( squeeze(COFF(10,:,:) ) ) 
+title( sprintf('OFF PROTOTYPE 10' ))
+suptitle('OFF polarity Prototypes')
 
 
-
-
-
-
-
-
-
-end
+end % end of function 
