62329c048db43ac31c12f4b0113ef78620f4b810
Colleen Pam Chen <colleenpamchen@gmail.com>
2017-06-20 18:17:25 -0700

    test files for implementing receptive fields

--- hots_nmf.m
+++ hots_nmf.m
@@ -1,4 +1,9 @@
 %% NMF of time-surfaces 
+% [ ] TIME STEPS ... it's ok if it's sparse. loop through time steps
+% instead of loop through events, that way you can reconstruct the SEQUENCE
+% of input events
+% [ ] SPATIAL RECEPTIVE FIELDS. implement the spatial neighborhood. 
+
 
 % load .dvs files
 Events1_v = load('sample_eDVS_data/pen_vertical.dvs');
@@ -12,17 +17,16 @@ Events= Events3_h;
 %  load('matfiles_prototypes/hand_h_CON.mat')
 %  load('matfiles_prototypes/hand_h_COFF.mat')
 
-
 %% video visualizing the events 
 % visualize_events(Events); 
 
 %% Batch incoming events: 
 num_events = size(Events,1) ; 
-batchsize = 1000;
+batchsize = 100;
 end_video = floor(num_events/batchsize) ;
 
 %% INITIAL prototypes 
-NUM_prototypes= 10;
+NUM_prototypes= 20;
 Cidx=[1:NUM_prototypes]; % Cn=10
 pixels= 128; 
 npixels= pixels*pixels;  % 16384
@@ -34,11 +38,16 @@ tau = 20000 ; % PARAMETER taken from Lagorce et. al.
 eidx=[1:batchsize];
 for i = 1:end_video              
     data = Events(eidx,:); 
-    [CON ,COFF, ONS, OFFS ]= hots(data, Cn_on, Cn_off, tau, Cidx) ; 
+%     [CON ,COFF, ONS, OFFS ]= hots(data, Cn_on, Cn_off, tau, Cidx) ; 
+[CON ,COFF, ONS, OFFS ]= test(data, Cn_on, Cn_off, tau, Cidx) ; 
             Cn_on = CON;
             Cn_off = COFF;
     eidx = eidx+batchsize; 
 end
+save CON.mat
+save COFF.mat
+save ONS.mat
+save OFFS.mat
 
 %% SAVE the prototypes as a video .avi file 
 savePrototype(CON,COFF,Cidx)
@@ -59,27 +68,54 @@ end
 
 %%  NMF 
 
-bf=5;
+bf=20;
 % ck = squeeze(COFF(10,:,:));
-[W,H,D]=nnmf(COFF(:,:),bf);
-figure; imagesc(W)
-h_disp = reshape(H,[bf,128,128]);
+[Woff,Hoff,Doff]=nnmf(COFF(:,:),bf);
+figure; imagesc(Woff)
+hoff_disp = reshape(Hoff,[bf,128,128]);
 for i = 1:bf
-figure; contour(squeeze(h_disp(i,:,:)))
+figure; contour(squeeze(hoff_disp(i,:,:)))
+title('NMF basis vectors. Parts based representation')
+end
+% now RECONSTRUCT input .......... how???? 
+reoff = Woff*Hoff ; % COFF
+reconoff=reshape(reoff,[length(Cidx),128,128]); 
+for i = 1:NUM_prototypes
+figure; contour(squeeze(reconoff(i,:,:)))
+end
+for ii =21:50
+    figure;
+    subplot(1,2,1)
+    contour(squeeze(pCOFF(ii,:,:)))
+    title('Original OFF_prototype')
+    subplot(1,2,2)
+    contour(squeeze(reconoff(ii,:,:)))
+    title('Reconstructed')
 end
 
 
+% ON 
 [Won,Hon,Don]=nnmf(CON(:,:),bf);
 figure; imagesc(Won)
 hon_disp = reshape(Hon,[bf,128,128]);
 for i = 1:bf
 figure; contour(squeeze(hon_disp(i,:,:)))
+title('basis vectors. parts based representation')
 end
-
-% now RECONSTRUCT input .......... how???? 
-
-
-re = W*H ; 
+reon = Won*Hon ; % COFF
+reconon=reshape(reon,[length(Cidx),128,128]); 
+for i = 1:NUM_prototypes
+figure; contour(squeeze(reconon(i,:,:)))
+end
+for ii = 131:150
+    figure;
+    subplot(1,2,1)
+    contour(squeeze(pCON(ii,:,:)))
+    title('Original ON_prototype')
+    subplot(1,2,2)
+    contour(squeeze(reconon(ii,:,:)))
+    title('Reconstructed')
+end
 
 
 
@@ -88,12 +124,5 @@ re = W*H ;
 % [W0,H0] = nnmf(X,5,'replicates',10,...
 %                    'options',opt,...
 %                    'algorithm','mult');
-
-
-
-
-
-
-
 
 
